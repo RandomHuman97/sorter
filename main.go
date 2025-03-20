8d983f358283fbdc977afa01a0bb48c174aa7a48
RandomHuman97 <103230029+RandomHuman97@users.noreply.github.com>
2025-03-19 20:29:55 -0400

    code

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/h2non/filetype"
+)
+
+func main() {
+	entries, err := os.ReadDir(".")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	for _, v := range entries {
+		if !v.Type().IsRegular() {
+			continue
+		}
+
+		buf, _ := os.ReadFile(v.Name())
+		kind, _ := filetype.Match(buf)
+		if kind.MIME.Type == "" {
+			fmt.Println("skipped unknown")
+			continue
+		}
+		new_dir := "./" + kind.MIME.Type
+
+		err := os.MkdirAll(new_dir, os.ModePerm)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		os.Rename("./"+v.Name(), new_dir+"/"+v.Name())
+
+	}
+
+}
